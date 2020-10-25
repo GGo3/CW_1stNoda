d0395dca070d16a8b0d551c80f7816c752ea4514
GGo3 <voloshynmv@gmail.com>
2020-10-25 11:20:16 +0200

    fixed main.js

--- js/calc.js
+++ js/calc.js
@@ -2,4 +2,5 @@ const myArr = require('./model');
 
 const owerweight = myArr.filter(person => Number(person.mass) > 100);
 const presonNames = owerweight.map(item => {return item.name});
-console.log(presonNames);
+
+module.exports = presonNames;
--- js/main.js
+++ js/main.js
@@ -1,4 +1,2 @@
-// массив имен людей у кого масса больше 100
-
 const data = require('./calc');
 console.log(data);
